6c9e6db0ccfd7c597078b3fd2081e4625692a854
zodiac-G12 <losershouyou@gmail.com>
2019-10-03 19:00:59 +0900

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const sleep = (n) => new Promise(resolve => setTimeout(resolve, n));
+var i = 0;
+var stopper = false;
+const speed = parseInt(location.href.split("#")[1]) || 100;
+
+function stop(){
+    stopper = !stopper;
+    init();
+}
+
+function init(){
+    (async()=>{
+        while(i<content.length&&!stopper){
+            document.getElementById("moji").innerHTML = content.slice(0,i+1);
+            document.scrollingElement.scrollTop = document.scrollingElement.scrollHeight;
+            await sleep(speed);
+            i=(i+1)|0;
+        }
+        sleep(60000);
+        location.href=location.href;
+    })();
+}
+
+window.addEventListener("DOMContentLoaded", init);
